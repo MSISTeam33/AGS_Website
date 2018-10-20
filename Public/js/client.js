d206ff754ed8da7a0776d2b8a50183173d784d36
atheffel <atheffel@iu.edu>
2018-10-20 15:37:05 -0400

    fixing errors on client page

--- Public/js/client.js
+++ Public/js/client.js
@@ -1,39 +1,13 @@
-var client = new Vue({
+var clientApp = new Vue({
   el: '#clientPage',
   data: {
     client: [],
 },
-
-methods: {
-  handleCommentForm(e){
-    const s = JSON.stringify(this.commentForm);
-    console.log(s);
-
-    fetch('api/client.php', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json; charset=utf-8"
-      },
-      body: s
-    })
-    .then( response => response.json())
-    .then( json => {this.comment.push(json)})
-    .catch( err => {
-      console.error('CLIENT POST ERROR:');
-      console.error(err);
-    })
-    this.commentForm = this.getEmptyCommentForm();
-  },
-  getEmptyCommentForm(){
-    return{
-      comment: ''
-    };
-  }
-},
+  
   created () {
     fetch('api/client.php')
     .then(response => response.json())
-    .then(json => {this.comment = json; console.log(json);})
+    .then(json => {this.client = json; console.log(json);})
     .catch(err=> {
       console.log('CLIENT FETCH ERROR:');
       console.log(err);
