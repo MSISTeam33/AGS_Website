022b7dbde23c239eb119c494ece7a353377793cc
Wanlin He <wanhe@indiana.edu>
2018-10-21 13:43:03 -0400

    error testing

--- Public/js/client.js
+++ Public/js/client.js
@@ -7,7 +7,7 @@ var clientApp = new Vue({
   created () {
     fetch('api/client.php')
     .then(response => response.json())
-    .then(json => {clientApp.clientList = json})
+    .then(json => {clientApp.clientList = json;})
     .catch(err=> {
       console.log('CLIENT FETCH ERROR:');
       console.log(err);
