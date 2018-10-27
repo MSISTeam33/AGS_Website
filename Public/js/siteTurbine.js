c38f112b3ba68d24b1f1b3c53174900d575e522b
pavthack <pavthack@iu.edu>
2018-10-27 16:27:15 -0400

    site turbine

--- Public/js/siteTurbine.js
+++ Public/js/siteTurbine.js
@@ -6,3 +6,43 @@ $('.data-expands').each(function(){
 		$(this).parent().find('.row-toggle').toggleClass('row-toggle-twist');
 	});
 });
+
+var siteApp = new Vue({
+  el: '#sitePage',
+  data: {
+      siteList: [{
+        'siteId' : '',
+        'clientId' : '',
+        'siteName' : '',
+        'siteDescription': '',
+        'primaryContact':'',
+        'capacity':'',
+        'commercialDate':'',
+        'addrLine1':'',
+        'addrCity':'',
+        'addrState':'',
+        'addrZip':'',
+        'addrCountry':''
+      }]
+  },
+  methods: {
+
+  }, //end of methods
+
+  created () {
+    // Do data fetch
+   const url = new URL(window.location.href);
+   const clientId = url.searchParams.get('clientId');
+   console.log('Client: '+ clientId);
+   //this.siteList.ClientId = clientId;
+
+   // TODO: Fetch task-specific data
+   fetch('api/site.php?clientId='+clientId)
+   .then( response => response.json() )
+   .then( json => {siteApp.siteList = json} )
+   .catch( err => {
+     console.error('SITE FETCH ERROR:');
+     console.error(err);
+   })
+  }
+})
