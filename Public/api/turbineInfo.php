bcebb816a2307419e434bbd9b7b0c05f9ba4ff3e
pavthack <pavthack@iu.edu>
2018-10-27 17:28:52 -0400

    turbine

--- Public/api/turbineInfo.php
+++ Public/api/turbineInfo.php
@@ -7,7 +7,7 @@ require '../../app/common.php';
     throw new Exception('Invalid Site ID');
   }
   //gets data that is fetched by the TurbineInfo Model from the SQL database
-  $turbineArr=Site::fetchTurbinesBySiteId($siteId);
+  $turbineArr=TurbineInfo::fetchTurbinesBySiteId($siteId);
   //convert to json and print
   $json = json_encode($turbineArr, JSON_PRETTY_PRINT);
   header('Content-type: application/json');
