73699f9c983c5961ad7e8d5ba0c60d8701b87396
pavthack <pavthack@iu.edu>
2018-10-27 19:00:21 -0400

    q

--- Public/js/turbineInfo.js
+++ Public/js/turbineInfo.js
@@ -1,35 +1,37 @@
-var turbineApp = new Vue({
-  el: '#turbinePage',
-  data: {
-      turbineList: [{
-        turbineDeployedId :'',
-        turbineId :'',
-        siteId :'',
-        serialNumber :'',
-        deployedDate :'',
-        totalFiredHours :'',
-        totalStarts :'',
-        lastPlannedOutageDate :'',
-        lastUnplannedOutageDate :''
-      }]
-  },
-  methods: {
+window.onload = function () {
+  var turbineApp = new Vue({
+    el: '#turbinePage',
+    data: {
+        turbineList: [{
+          turbineDeployedId :'',
+          turbineId :'',
+          siteId :'',
+          serialNumber :'',
+          deployedDate :'',
+          totalFiredHours :'',
+          totalStarts :'',
+          lastPlannedOutageDate :'',
+          lastUnplannedOutageDate :''
+        }]
+    },
+    methods: {
 
-  }, //end of methods
+    }, //end of methods
 
-  created () {
-    // Do data fetch
-   const url = new URL(window.location.href);
-   const siteId = url.searchParams.get('siteId');
-   console.log('Site: '+ siteId);
+    created () {
+      // Do data fetch
+     const url = new URL(window.location.href);
+     const siteId = url.searchParams.get('siteId');
+     console.log('Site: '+ siteId);
 
-   // TODO: Fetch task-specific data
-   fetch('api/turbineInfo.php?siteId='+siteId)
-   .then( response => response.json() )
-   .then( json => {turbineApp.turbineList = json} )
-   .catch( err => {
-     console.error('TURBINE FETCH ERROR:');
-     console.error(err);
-   })
-  }
-})
+     // TODO: Fetch task-specific data
+     fetch('api/turbineInfo.php?siteId='+siteId)
+     .then( response => response.json() )
+     .then( json => {turbineApp.turbineList = json} )
+     .catch( err => {
+       console.error('TURBINE FETCH ERROR:');
+       console.error(err);
+     })
+    }
+  })
+}
