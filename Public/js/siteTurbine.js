158d1bdb0967ed3180b89f2f9653080d74c6677b
pavthack <pavthack@iu.edu>
2018-10-27 16:44:16 -0400

    site turbine

--- Public/js/siteTurbine.js
+++ Public/js/siteTurbine.js
@@ -1,48 +1,8 @@
-var siteApp = new Vue({
-  el: '#sitePage',
-  data: {
-      siteList: [{
-        'siteId' : '',
-        'clientId' : '',
-        'siteName' : '',
-        'siteDescription': '',
-        'primaryContact':'',
-        'capacity':'',
-        'commercialDate':'',
-        'addrLine1':'',
-        'addrCity':'',
-        'addrState':'',
-        'addrZip':'',
-        'addrCountry':''
-      }]
-  },
-  methods: {
-    showTurbines(){
-    	$(this).click(function(){
-    		$(this).toggleClass('row-active');
-    		$(this).parent().find('.expandable').toggleClass('row-open');
-    		$(this).parent().find('.row-toggle').toggleClass('row-toggle-twist');
-    	});
-    }
-  }, //end of methods
-
-  created () {
-    // Do data fetch
-   const url = new URL(window.location.href);
-   const clientId = url.searchParams.get('clientId');
-   console.log('Client: '+ clientId);
-   //this.siteList.ClientId = clientId;
-
-   // TODO: Fetch task-specific data
-   fetch('api/site.php?clientId='+clientId)
-   .then( response => response.json() )
-   .then( json => {siteApp.siteList = json} )
-   .catch( err => {
-     console.error('SITE FETCH ERROR:');
-     console.error(err);
-   });
-  }
-});
-
 // Expandable Data Table
-$('.data-expands').each();
+$('.data-expands').each(function(){
+	$(this).click(function(){
+		$(this).toggleClass('row-active');
+		$(this).parent().find('.expandable').toggleClass('row-open');
+		$(this).parent().find('.row-toggle').toggleClass('row-toggle-twist');
+	});
+});
