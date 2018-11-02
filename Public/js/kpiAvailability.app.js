0da6213b6f81e96a4588c7994783008583885897
smalpedd <smalpedd@iu.edu>
2018-11-02 00:26:37 -0400

    testing availability

--- Public/js/kpiAvailability.app.js
+++ Public/js/kpiAvailability.app.js
@@ -43,6 +43,32 @@ var kpiAvailabilityApp = new Vue({
                       text: 'Availability Percentage'
                     }
                   },
+                  plotOptions: {
+                      area: {
+                          fillColor: {
+                              linearGradient: {
+                                  x1: 0,
+                                  y1: 0,
+                                  x2: 0,
+                                  y2: 1
+                              },
+                              stops: [
+                                  [0, Highcharts.getOptions().colors[0]],
+                                  [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                              ]
+                          },
+                          marker: {
+                              radius: 2
+                          },
+                          lineWidth: 1,
+                          states: {
+                              hover: {
+                                  lineWidth: 1
+                              }
+                          },
+                          threshold: null
+                      }
+                  },
                   title: {
                     text: 'Scatter Plot of Availability'
                   },
